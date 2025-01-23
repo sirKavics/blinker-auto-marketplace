f8b673febfcb718d549d2c6285d05b62bf7c4893
sirkavics <falf555@gmail.com>
2025-01-23 00:29:56 -0500

    index page 576px and 420px responsivness

--- findyourcar.js
+++ findyourcar.js
@@ -1,7 +1,5 @@
-// apiKey = 
-// apiAdress = 
-
-// console.log(carData)
+// apiKey = ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
+// apiAdress = https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
 
 async function main() {
     const cars = await fetch(`https://parseapi.back4app.com/classes/Car_Model_List?count=1&limit=10`)
@@ -10,4 +8,4 @@ async function main() {
 
 main();
 
-console.log(carsData)
+console.log(fetch("https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20="))
