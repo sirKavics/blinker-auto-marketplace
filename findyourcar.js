8e957e6c591fbd9c16980dc8bf976c453e8e06fb
sirkavics <falf555@gmail.com>
2025-01-24 16:59:08 -0500

    find your car page API function

--- findyourcar.js
+++ findyourcar.js
@@ -1,10 +1,20 @@
 function openMenu() {
-    document.body.classList += " menu--open"
-    }
-    
-function closeMenu() {
-        document.body.classList.remove('menu--open')
-    }
+  document.body.classList += " menu--open";
+};
 
-// API Link: https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
-// API Key: Authorization: Bearer ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+};
+
+// API url: https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
+// API authorization key: Authorization: Bearer ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
+
+async function main() {
+const response = await fetch("https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=", {
+  headers: {
+    "Authorization": "Bearer ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20="
+  }
+})
+};
+
+main();
