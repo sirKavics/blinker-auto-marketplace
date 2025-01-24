d54573862a9c7e04d86cf3f5d6eebd20eba28564
sirkavics <falf555@gmail.com>
2025-01-24 03:17:47 -0500

    find your car page burger menu for 996px responsiveness

--- findyourcar.js
+++ findyourcar.js
@@ -1,11 +1,10 @@
-// apiKey = ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
-// apiAdress = https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
+function openMenu() {
+    document.body.classList += " menu--open"
+    }
+    
+function closeMenu() {
+        document.body.classList.remove('menu--open')
+    }
 
-async function main() {
-    const cars = await fetch(`https://parseapi.back4app.com/classes/Car_Model_List?count=1&limit=10`)
-    const carsData = await cars.json();
-}
-
-main();
-
-console.log(fetch("https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20="))
+// API Link: https://auto.dev/api/listings?apikey=ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
+// API Key: Authorization: Bearer ZrQEPSkKZmFsZjU1NUBnbWFpbC5jb20=
