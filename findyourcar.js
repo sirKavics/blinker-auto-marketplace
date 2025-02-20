e052d32917887be7730276490221bfc339c93bcb
sirkavics <falf555@gmail.com>
2025-02-19 20:12:28 -0500

    deleted find your car page price filter function

--- findyourcar.js
+++ findyourcar.js
@@ -35,20 +35,6 @@ const handleDrag = (button, isMin) => {
 }
 
 // SEARCH AND RENDER CARS
-let selectedSearchType = "name";
-
-function selectOption(index) {
-  const options = document.querySelectorAll(".toggle__option");
-  const slider = document.querySelector(".toggle__slider");
-  const searchTypes = ["name","model","year"];
-  selectedSearchType = searchTypes[index] || "name";
-
-  slider.style.transform = `translateX(${index * 100}%)`;
-  options.forEach(option => option.classList.remove("toggle__active"));
-  options[index].classList.add("toggle__active");
-
-  console.log(`Search type changed to: ${selectedSearchType}`)
-}
 
 async function renderCars() {
   const searchButton = document.querySelector(".btn__search");
