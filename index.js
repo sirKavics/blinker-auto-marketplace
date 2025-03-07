2e6719722e0ea1ffedd76a1ab0e38def09bfefa1
sirkavics <falf555@gmail.com>
2025-03-07 15:27:06 -0500

    restructuring js functions for landing page and fing your car page functionality

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function openMenu() {
   function closeMenu() {
     document.body.classList.remove("menu--open");
   }
+
+  let selectedSearchType = "name";
+
+function selectOption(index) {
+  const options = document.querySelectorAll(".toggle__option");
+  const slider = document.querySelector(".toggle__slider");
+  const searchTypes = ["name","model","year"];
+
+  selectedSearchType = searchTypes[index] || "name";
+
+  slider.style.transform = `translateX(${index * 100}%)`;
+  options.forEach((option) => option.classList.remove("toggle__active"));
+  options[index].classList.add("toggle__active");
+
+  console.log(`Search type changed to: ${selectedSearchType}`)
+}
